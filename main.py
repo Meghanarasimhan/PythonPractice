d5f9a76c8f64e8bfc413b5634e9de09a9fb10941
Meghashree_BL <meghashreebl@gmail.com>
2025-05-16 19:30:24 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from loguru import logger
+
+
+# labourName=["mahesh","ramesh", "Mithilesh", "Sumesh"]
+
+# for name in labourName:
+#     logger.info(f"Labour {name}")
+
+# for i in range(len(labourName)):
+#     logger.info(f"Labour {i+1} name is {labourName[i]}")
+
+#Printing * like triangle
+# for i in range(5,0,-1):
+#     logger.info(i * " *")
+
+
+
+# #Printing even numbers
+# for i in range(101):
+#     if(i!=0):
+#         if(i%2==0):
+#             logger.info(i)
+
+#Counting the in a paragraph
+count=0
+paragraph="Ralph Kimball founded the Kimball Group. Since the mid-1980s, he has been the data warehouse and business intelligence industry’s thought leader on the dimensional approach. He has educated tens of thousands of IT professionals. The Toolkit books written by Ralph and his colleagues have been the industry’s best sellers since 1996. Prior to working at Metaphor and founding Red Brick Systems, Ralph coinvented the Star workstation, the fi rst commercial product with windows, icons, and a mouse, at Xerox’s Palo Alto Research Center (PARC). Ralph has a PhD in electrical engineering from Stanford University"
+paragraph=paragraph.lower()
+paragraph=paragraph.split(" ")
+for i in paragraph:
+    if i== "the":
+        count+=1
+    else:
+        continue
+logger.info(count)
